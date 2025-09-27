cf950db342d271f92b42e43dd38b49a4522d1edc
Harry Forson <linotopo08@gmail.com>
2025-09-27 14:56:13 +0000

    commented on code

--- fns_and_dsa/arithmetic_operations.py
+++ fns_and_dsa/arithmetic_operations.py
@@ -1,5 +1,6 @@
-
+# Define the perform_operation function
 def perform_operation(num1, num2, operation):
+    # Perform the operation based on the input
     if operation == 'add':
         return num1 + num2
     elif operation == 'subtract':
